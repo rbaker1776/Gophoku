b98c29bf423c11abe877ace6946fd53fdeed0d9e
Ryan Baker <rbaker@xtier.com>
2025-10-17 11:31:58 -0400

    Added basic CLI abilityz

---
+++ cmd/gen.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"sudoku/internal/generator"
+	"time"
+	"github.com/spf13/cobra"
+)
+
+var (
+	numPuzzles int
+	clueCount  int
+	timeout    time.Duration
+)
+
+func init() {
+	genCmd := &cobra.Command{
+		Use:   "gen",
+		Short: "Generate Sudoku puzzles",
+		Long: `Generate one or more Sudoku puzzles with a specified difficulty level.
+
+Examples:
+  sudoku gen --clueCount 40
+  sudoku gen -n 5 --clueCount 30
+  sudoku gen --clueCount 20 --timeout 15s`,
+		RunE: runGen,
+	}
+
+	genCmd.Flags().IntVarP(&numPuzzles, "number", "n", 1, "Number of puzzles to generate")
+	genCmd.Flags().IntVarP(&clueCount, "clueCount", "c", generator.DefaultClueCount, "Number of clues 17-80")
+	genCmd.Flags().DurationVar(&timeout, "timeout", 10*time.Second, "Generation timeout per puzzle")
+
+	rootCmd.AddCommand(genCmd)
+}
+
+func runGen(cmd *cobra.Command, args []string) error {
+	for i := 0; i < numPuzzles; i++ {
+		opts := generator.DefaultOptions(clueCount)
+		opts.Timeout = timeout
+		gen := generator.New(opts)
+
+		puzzle, solution, err := gen.Generate()
+		if err != nil {
+			return fmt.Errorf("generation failed: %w", err)
+		}
+
+		fmt.Println("Puzzle:")
+		fmt.Println(puzzle.Format())
+		fmt.Println("\nSolution:")
+		fmt.Println(solution.Format())
+		fmt.Println()
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"sudoku/internal/generator"
-)
+import "sudoku/cmd"
 
 func main() {
-	board, solution, err := generator.GenerateWithClueCount(21)
-	if err == nil {
-		fmt.Println(board.Format())
-		fmt.Println(solution.Format())
-	} else {
-		fmt.Println(err)
-	}
+    cmd.Execute()
 }
